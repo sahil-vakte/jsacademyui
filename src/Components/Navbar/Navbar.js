b5ff6e1c45515972e888e6e60eb364ccb0a7a25a
Sahil <sahilvaktepatil123@gmail.com>
2023-06-28 22:14:57 +0530

    Responsive Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { useState } from "react";
+import "./Navbar.css";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdClose } from 'react-icons/md';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-export default Navbar
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+  return (
+    <nav className="navbar">
+    <div className="responsive-logo">
+      <div className="navbar-logo">
+        <a href="/">JS Academy</a>
+      </div>
+      <div className="navbar-mobile-menu" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ?  <MdClose/> : <GiHamburgerMenu/> }
+      </div>
+      </div>
+      <ul className={`navbar-nav ${isMobileMenuOpen ? "open" : ""}`}>
+        <li className="nav-item">
+          <a href="/" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/about" className="nav-link">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/contact" className="nav-link">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
