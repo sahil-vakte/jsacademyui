bde81dd2793c6607db13aa313a06372a278f94a5
Sahil <sahilvaktepatil123@gmail.com>
2023-06-30 10:30:00 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Components/HomePage/HomePage';
+import Syllabus from './Components/Syllabus/Syllabus';
 
 function App() {
   return (
     <div className="body">
-     <Navbar/>
-     <HomePage/>
-    </div>
+    <Navbar/>
+    <Routes>
+    <Route path='/' element=<HomePage/>/>
+    <Route path='/syllabus' element=<Syllabus/>/>
+  </Routes>
+  </div>
   );
 }
 
---
+++ src/Components/Syllabus/Syllabus.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Syllabus.css"
+
+const Syllabus = () => {
+  return (
+    <div>Syllabus</div>
+  )
+}
+
+export default Syllabus
